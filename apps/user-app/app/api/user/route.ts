6936583b2edebfc9b7709dbfb891b52e0ec7d201
subhamnayak76 <subhamnayak9638@gmail.com>
2024-08-16 20:22:54 +0530

    turbo-setup

---
+++ apps/user-app/app/api/user/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server"
+import { PrismaClient } from "@repo/db/client";
+
+const client = new PrismaClient();
+
+export const GET = async () => {
+    await client.user.create({
+        data: {
+            email: "amnnmnb",
+            name: "adsads"
+        }
+    })
+    return NextResponse.json({
+        message: "hi there"
+    })
+}
