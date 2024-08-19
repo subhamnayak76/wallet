e627e33d62ec77e217ebef38e0ac6fe739d0ad9a
subhamnayak76 <subhamnayak9638@gmail.com>
2024-08-19 18:41:54 +0530

    frontend done

--- apps/user-app/app/api/user/route.ts
+++ apps/user-app/app/api/user/route.ts
@@ -3,13 +3,27 @@ import { NextResponse } from "next/server";
 import { authOptions } from "../../lib/auth";
 
 export const GET = async () => {
+    try {
+    
     const session = await getServerSession(authOptions);
     if (session.user) {
         return NextResponse.json({
             user: session.user
         })
     }
+    
+}
+    catch(e) {  
+        console.error(e);
+        return NextResponse.json({
+            message: "You are not logged in"
+        }, {
+            status: 403
+        })
+    }
+
     return NextResponse.json({
+        
         message: "You are not logged in"
     }, {
         status: 403
